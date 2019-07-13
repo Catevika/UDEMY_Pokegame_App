59db0ec850fa5cca665c63ea028d733eddb310d6
Catevika <dominique.bello@outlook.fr>
2019-07-13 19:08:29 +0200

    Updates

---
+++ precache-manifest.caf58bcda696990c62988e2a77a0bf48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f7678e078a20ba024ad2990fac8861c",
+    "url": "/UDEMY_Pokegame_App/index.html"
+  },
+  {
+    "revision": "4b681b452e6feadacabe",
+    "url": "/UDEMY_Pokegame_App/static/css/main.8a3d8011.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/UDEMY_Pokegame_App/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "4b681b452e6feadacabe",
+    "url": "/UDEMY_Pokegame_App/static/js/main.f5008661.chunk.js"
+  },
+  {
+    "revision": "5de41f5a68a0ad89ad62",
+    "url": "/UDEMY_Pokegame_App/static/js/runtime~main.def29a77.js"
+  }
+]);
